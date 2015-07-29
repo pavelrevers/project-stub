b5e0cf5430529ab175054f05eb0054df6ec2b053
Vladimir Grinenko <i@tadatuta.com>
2015-07-30 02:59:41 +0300

    update bemjson

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -7,24 +7,10 @@
         { elem: 'css', url: '_index.ie.css', ie: 'gte IE 6' },
         { elem: 'meta', attrs: { name: 'description', content: '' }}
     ],
-    content:[
+    content: [
         {
-            block: 'header',
-            content: [
-                'header content goes here'
-            ]
-        },
-        {
-            block: 'content',
-            content: [
-                'main content'
-            ]
-        },
-        {
-            block: 'footer',
-            content: [
-                'footer content goes here'
-            ]
+            block: 'total-amount',
+            total: 100500
         },
         { block: 'i-jquery', mods: { version: '1.8.3' } },
         { elem: 'js', url: '_index.js' }
