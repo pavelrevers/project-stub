427506478b3c9559f8e5ed19ad47029ae71621c4
Vladimir Grinenko <i@tadatuta.com>
2015-11-15 23:56:29 +0300

    Second variant

--- common.blocks/app/app.js
+++ common.blocks/app/app.js
@@ -6,13 +6,7 @@ provide(BEMDOM.decl(this.name, {
             inited: function() {
                 console.log('app inited');
 
-                this._table = this.findBlockInside('table');
                 this._header = this.findBlockInside('header');
-
-                Checkbox.on(this._table.domElem, { modName: 'checked', modVal: '*' }, this.onCheckboxToggled, this);
-            },
-            '': function() {
-                Checkbox.un(this._table.domElem, { modName: 'checked', modVal: '*' });
             }
         }
     },
@@ -21,6 +15,10 @@ provide(BEMDOM.decl(this.name, {
 
         this._header.setMod('color', checkbox.getMod('type'));
         BEMDOM.update(this.elem('header'), checkbox.getVal());
+    }
+}, {
+    live: function() {
+        this.liveInitOnBlockInsideEvent({ modName: 'checked', modVal: '*' }, 'checkbox', this.prototype.onCheckboxToggled);
     }
 }));
 
